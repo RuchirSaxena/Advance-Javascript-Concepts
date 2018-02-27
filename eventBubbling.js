0550771e8c18d45d2d58650cc86e4957825e68a6
Ruchir Saxena <ruchir.saxena@irissoftware.com>
2018-02-27 17:41:55 +0530

    EventBubblingAndCapturing

---
+++ eventBubbling.js
@@ -0,0 +1,8 @@
+
+document.getElementById('parent').addEventListener('click',function () {
+   alert("parent Clicked");
+},true);
+
+document.getElementById('child').addEventListener('click',function () {
+   alert('child clicked');
+},true);
